09d3d1c0d9f2e2c9fe4be901327ef97d791ff929
Chloe-YT <60844460+Chloe-YT@users.noreply.github.com>
2020-04-29 23:34:29 +0800

    初步记账界面
    记账界面初步编写，待完善。

--- app/src/main/java/com/myapplication/Fragment2.java
+++ app/src/main/java/com/myapplication/Fragment2.java
@@ -1,15 +1,20 @@
 package com.myapplication;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Fragment2 extends Fragment {
 
     private View mView;
-
+    private Button incomeTv;    //收入按钮
+    private Button outcomeTv;   //支出按钮
 
     @Override
     public View onCreateView(LayoutInflater inflater,  ViewGroup container,  Bundle savedInstanceState) {
@@ -17,6 +22,24 @@ public class Fragment2 extends Fragment {
         if(mView == null) {
             mView = inflater.inflate(R.layout.fragment2,container,false);
         }
+
+        incomeTv=(Button) mView.findViewById(R.id.income_tv);
+        incomeTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //支出按钮监听器
+                Log.d("Fragment2","支出");
+            }
+        });
+
+        outcomeTv = (Button) mView.findViewById(R.id.outcome_tv);
+        outcomeTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //收入按钮监听器
+                Log.d("Fragment2","收入");
+            }
+        });
 
         return mView;
     }
