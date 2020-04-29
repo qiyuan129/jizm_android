79633fb18c3b5bb09decc9628e8b96dcdbef0453
Dante <1139030617@qq.com>
2020-04-29 17:43:01 +0800

    首页增加两个滑动页面

--- app/src/main/java/com/myapplication/Fragment1.java
+++ app/src/main/java/com/myapplication/Fragment1.java
@@ -3,15 +3,27 @@ package com.myapplication;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import util.MyDatabaseHelper;
 
 public class Fragment1 extends Fragment {
     private View mView;
+    private ViewPager viewPager;
+    private Button trend;
+    private Button category;
+    private Fragment1_1 fragment1_1;
+    private Fragment1_2 fragment1_2;
+    private List<Fragment> list;
 
 
     private MyDatabaseHelper dbHelper;
@@ -35,6 +47,34 @@ public class Fragment1 extends Fragment {
 //                dbHelper.getWritableDatabase();
 //            }
 //        });
+
+        fragment1_1 = new Fragment1_1();
+        fragment1_2 = new Fragment1_2();
+        list = new ArrayList<>();
+        list.add(fragment1_1);
+        list.add(fragment1_2);
+
+        viewPager = (ViewPager) mView.findViewById(R.id.viewpager);
+        MyViewPagerAdapter adapter = new MyViewPagerAdapter(getChildFragmentManager());
+        viewPager.setAdapter(adapter);
+
+
+        trend=(Button) mView.findViewById(R.id.trend);
+        trend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewPager.setCurrentItem(0);
+            }
+        });
+        category=(Button) mView.findViewById(R.id.category);
+        category.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewPager.setCurrentItem(1);
+            }
+        });
+
+
         dbHelper.getWritableDatabase();
 
 
@@ -54,4 +94,26 @@ public class Fragment1 extends Fragment {
 
 
     }
+
+
+
+    public class MyViewPagerAdapter extends FragmentPagerAdapter {
+
+        public MyViewPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        //返回每个position对应的Fragment对象
+        @Override
+        public Fragment getItem(int position) {
+            return list.get(position);
+        }
+
+        //返回list的长度，也就是Fragment对象的个数
+        @Override
+        public int getCount() {
+            return list.size();
+        }
+    }
+
 }
