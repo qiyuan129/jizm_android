5a63bc37e1ebecd07ea0ea1d01e190364943df89
Qi <154496613@qq.com>
2020-04-30 18:02:51 +0800

    月趋势列表 分类top10列表静态数据展示

--- app/src/main/java/com/myapplication/Fragment1_1.java
+++ app/src/main/java/com/myapplication/Fragment1_1.java
@@ -1,23 +1,117 @@
 package com.myapplication;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Fragment1_1 extends Fragment {
 
     private View mView;
+    private List<TrendListItem> trendList = new ArrayList<>();
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         //注意View对象的重复使用，以便节省资源
-        if(mView == null) {
-            mView = inflater.inflate(R.layout.fragment1_1,container,false);
+        if (mView == null) {
+            mView = inflater.inflate(R.layout.fragment1_1, container, false);
         }
+        initTrentList();
+        ListView lview = (ListView) mView.findViewById(R.id.TrendList);
+        /*lview.setOnTouchListener(new View.OnTouchListener() {
 
+            public boolean onTouch(View v, MotionEvent event) {
+                ListView lview = (ListView) mView.findViewById(R.id.TrendList);
+                // TODO Auto-generated method stub
+                lview.getParent().requestDisallowInterceptTouchEvent(true);
+                return false;
+            }
+        });*/
+        TrendListAdapter adapter = new TrendListAdapter(getActivity(), R.layout.trendlist_item, trendList);
+        lview.setAdapter(adapter);
         return mView;
     }
+
+    private void initTrentList() {
+        for (int i = 0; i < 2; i++) {
+            TrendListItem t1 = new TrendListItem("1月", "12345.99", "1234.00", "1234.00");
+            trendList.add(t1);
+            TrendListItem t2 = new TrendListItem("2月", "123451.00", "123451.00", "123451.00");
+            trendList.add(t2);
+            TrendListItem t3 = new TrendListItem("3月", "123451.00", "123451.00", "123451.00");
+            trendList.add(t3);
+            TrendListItem t4 = new TrendListItem("4月", "123451.00", "123451.00", "123451.00");
+            trendList.add(t4);
+        }
+        TrendListItem t4 = new TrendListItem("4月", "123451.00", "123451.00", "123451.00");
+        trendList.add(t4);
+    }
+}
+
+
+class TrendListItem
+{
+        private String month;
+        private String income;
+        private String outcome;
+        private String balance;
+
+        public TrendListItem(String month,String income,String outcome,String balance)
+        {
+            this.month=month;
+            this.income=income;
+            this.outcome=outcome;
+            this.balance=balance;
+        }
+
+        public String getMonth() {
+            return month;
+        }
+
+        public String getIncome() {
+            return income;
+        }
+
+        public String getOutcome() {
+            return outcome;
+        }
+
+        public String getBalance() {
+            return balance;
+        }
+}
+
+class TrendListAdapter extends ArrayAdapter<TrendListItem>
+{
+        private int id;
+        public TrendListAdapter(Context context, int textid, List<TrendListItem>objects){
+            super(context,textid,objects);
+            id=textid;
+        }
+
+        @Override
+        public View getView (int position, View convertView, ViewGroup parent)
+        {
+            TrendListItem trendListItem = getItem(position);
+            View view = LayoutInflater.from(getContext()).inflate(id,parent,false);
+            TextView month = (TextView) view.findViewById(R.id.trendmonth);
+            TextView income = (TextView) view.findViewById(R.id.trendincome);
+            TextView outcome = (TextView) view.findViewById(R.id.trendoutcome);
+            TextView balance = (TextView) view.findViewById(R.id.trendbalance);
+            month.setText(trendListItem.getMonth());
+            income.setText(trendListItem.getIncome());
+            outcome.setText(trendListItem.getOutcome());
+            balance.setText(trendListItem.getBalance());
+            return view;
+        }
 }
--- app/src/main/java/com/myapplication/Fragment1_2.java
+++ app/src/main/java/com/myapplication/Fragment1_2.java
@@ -1,24 +1,114 @@
 package com.myapplication;
-
+import android.content.Context;
+import android.content.SearchRecentSuggestionsProvider;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Fragment1_2 extends Fragment {
-
+public class Fragment1_2 extends Fragment
+{
 
     private View mView;
+    private List<CategoryListItem> categoryList = new ArrayList<>();
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         //注意View对象的重复使用，以便节省资源
-        if(mView == null) {
-            mView = inflater.inflate(R.layout.fragment1_2,container,false);
+        if (mView == null) {
+            mView = inflater.inflate(R.layout.fragment1_2, container, false);
         }
+        initCategoryList();
+        ListView lview = (ListView) mView.findViewById(R.id.CategoryList);
+        /*lview.setOnTouchListener(new View.OnTouchListener() {
 
+            public boolean onTouch(View v, MotionEvent event) {
+                ListView lview = (ListView) mView.findViewById(R.id.CategoryList);
+                // TODO Auto-generated method stub
+                lview.getParent().requestDisallowInterceptTouchEvent(true);
+                return false;
+            }
+        });*/
+        CategoryListAdapter adapter = new CategoryListAdapter(getActivity(), R.layout.categorylist_item, categoryList);
+        lview.setAdapter(adapter);
         return mView;
+    }
+
+    private void initCategoryList() {
+        int i;
+        for (i = 0; i < 5; i++) {
+            CategoryListItem c1 = new CategoryListItem(String.valueOf(i*2+1), "购物", "1234.00", "2020-2-7");
+            categoryList.add(c1);
+            CategoryListItem c2 = new CategoryListItem(String.valueOf(i*2+2), "饮食", "123451.00", "2020-2-7");
+            categoryList.add(c2);
+        }
+        CategoryListItem c2 = new CategoryListItem(String.valueOf(i*2+2), "饮食", "123451.00", "2020-2-7");
+        categoryList.add(c2);
+    }
+}
+
+
+class CategoryListItem
+{
+    private String order;
+    private String categoryname;
+    private String money;
+    private String date;
+
+    public CategoryListItem(String order,String categoryname,String money,String date)
+    {
+        this.order=order;
+        this.categoryname=categoryname;
+        this.money=money;
+        this.date=date;
+    }
+
+    public String getOrder() {
+        return order;
+    }
+
+    public String getCategoryname() {
+        return categoryname;
+    }
+
+    public String getMoney() {
+        return money;
+    }
+
+    public String getDate() {
+        return date;
+    }
+}
+
+class CategoryListAdapter extends ArrayAdapter<CategoryListItem>
+{
+    private int id;
+    public CategoryListAdapter(Context context, int textid, List<CategoryListItem>objects){
+        super(context,textid,objects);
+        id=textid;
+    }
+
+    @Override
+    public View getView (int position, View convertView, ViewGroup parent)
+    {
+        CategoryListItem categoryListItem = getItem(position);
+        View view = LayoutInflater.from(getContext()).inflate(id,parent,false);
+        TextView month = (TextView) view.findViewById(R.id.order);
+        TextView income = (TextView) view.findViewById(R.id.categoryname);
+        TextView outcome = (TextView) view.findViewById(R.id.money);
+        TextView balance = (TextView) view.findViewById(R.id.date);
+        month.setText(categoryListItem.getOrder());
+        income.setText(categoryListItem.getCategoryname());
+        outcome.setText(categoryListItem.getMoney());
+        balance.setText(categoryListItem.getDate());
+        return view;
     }
 }
