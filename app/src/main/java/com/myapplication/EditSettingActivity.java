f87bde9b264e9d61642d6bd7c95f70ac6d49ac9d
Baixuangezhu <2046687503@qq.com>
2020-05-02 19:59:58 +0800

    写了个人信息修改页面和设置页面

---
+++ app/src/main/java/com/myapplication/EditSettingActivity.java
@@ -0,0 +1,17 @@
+package com.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditSettingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_setting);
+    }
+
+
+
+
+}
