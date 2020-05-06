3a2341c0146849bed09dff39f22d07cad73032dd
Chloe-YT <60844460+Chloe-YT@users.noreply.github.com>
2020-05-06 22:20:26 +0800

    修改bug、界面细节展示优化
    修改bug、界面细节展示优化

--- app/src/main/java/com/myapplication/CategoryEditActivity.java
+++ app/src/main/java/com/myapplication/CategoryEditActivity.java
@@ -46,13 +46,13 @@ public class CategoryEditActivity extends AppCompatActivity implements View.OnCl
     private ImageView backIv;      //返回键
     private ImageView addIv;      //添加按钮
 
-    private String edit_category_name = "";
-
     private String[] category_outcome = {"餐饮美食", "服饰美容", "生活日用", "充值缴费",
             "交通出行", "通讯物流", "休闲娱乐", "医疗保健", "住房物业", "文体教育",
             "酒店旅行", "爱车养车", "其他"};
 
     private String[] category_income = {"投资理财", "经营所得", "奖金红包", "工资", "生活费"};
+
+    private String edit_category_name = "";
 
     private int category_id = 1;
     private int user_id = 1;
@@ -88,27 +88,33 @@ public class CategoryEditActivity extends AppCompatActivity implements View.OnCl
             public void onClick(int position) {
                 CategoryDAO categoryDAO = new CategoryDAOImpl();
                 categoryList = categoryDAO.listCategory();
+                List<String> outcome_category_name1 = new ArrayList<>();
+                List<String> income_category_name1 = new ArrayList<>();
                 List<Integer> outcome_category_id1 = new ArrayList<>();
                 List<Integer> income_category_id1 = new ArrayList<>();
+                String category_name1;
 
                 for (int j = 0; j < categoryList.size(); j++) {
                     Category category = (Category)categoryList.get(j);
                     if (category.getType() == 0) {
+                        outcome_category_name1.add(category.getCategory_name());
                         outcome_category_id1.add(category.getCategory_id());
                     } else {
+                        income_category_name1.add(category.getCategory_name());
                         income_category_id1.add(category.getCategory_id());
                     }
                 }
 
-                Category category = categoryList.get(position);
                 if (isIncome == 0) {
                     category_id = outcome_category_id1.get(position);
+                    category_name1 = outcome_category_name1.get(position);
                 } else {
                     category_id = income_category_id1.get(position);
+                    category_name1 = income_category_name1.get(position);
                 }
                 new MaterialDialog.Builder(CategoryEditActivity.this)
                         .title("修改分类")
-                        .input(category.getCategory_name(), edit_category_name, new MaterialDialog.InputCallback() {
+                        .input(category_name1, null, new MaterialDialog.InputCallback() {
                             @Override
                             public void onInput(@NonNull MaterialDialog dialog, CharSequence input) {
                                 edit_category_name = input.toString();
@@ -125,6 +131,7 @@ public class CategoryEditActivity extends AppCompatActivity implements View.OnCl
                                 CategoryDAO categoryDAO = new CategoryDAOImpl();
                                 categoryDAO.updateCategory(category);
                                 initCategory();
+                                edit_category_name = "";
                             }
                         })
                         .onNeutral(new MaterialDialog.SingleButtonCallback() {
@@ -166,23 +173,39 @@ public class CategoryEditActivity extends AppCompatActivity implements View.OnCl
                     public void onClick(int position) {
                         CategoryDAO categoryDAO = new CategoryDAOImpl();
                         categoryList = categoryDAO.listCategory();
+                        List<String> outcome_category_name2 = new ArrayList<>();
+                        List<String> income_category_name2 = new ArrayList<>();
                         List<Integer> outcome_category_id2 = new ArrayList<>();
                         List<Integer> income_category_id2 = new ArrayList<>();
+                        String category_name2;
 
+//                if (outcome_category_id1 != null) {
+//                    outcome_category_id1.clear();
+//                }
+//                if (income_category_id1 != null) {
+//                    income_category_id1.clear();
+//                }
                         for (int j = 0; j < categoryList.size(); j++) {
                             Category category = (Category)categoryList.get(j);
                             if (category.getType() == 0) {
+                                outcome_category_name2.add(category.getCategory_name());
                                 outcome_category_id2.add(category.getCategory_id());
                             } else {
+                                income_category_name2.add(category.getCategory_name());
                                 income_category_id2.add(category.getCategory_id());
                             }
                         }
 
-                        Category category = categoryList.get(position);
-                        category_id = income_category_id2.get(position);
+                        if (isIncome == 0) {
+                            category_id = outcome_category_id2.get(position);
+                            category_name2 = outcome_category_name2.get(position);
+                        } else {
+                            category_id = income_category_id2.get(position);
+                            category_name2 = income_category_name2.get(position);
+                        }
                         new MaterialDialog.Builder(CategoryEditActivity.this)
                                 .title("修改分类")
-                                .input(category.getCategory_name(), edit_category_name, new MaterialDialog.InputCallback() {
+                                .input(category_name2, null, new MaterialDialog.InputCallback() {
                                     @Override
                                     public void onInput(@NonNull MaterialDialog dialog, CharSequence input) {
                                         edit_category_name = input.toString();
@@ -199,6 +222,7 @@ public class CategoryEditActivity extends AppCompatActivity implements View.OnCl
                                         CategoryDAO categoryDAO = new CategoryDAOImpl();
                                         categoryDAO.updateCategory(category);
                                         initCategory();
+                                        edit_category_name = "";
                                     }
                                 })
                                 .onNeutral(new MaterialDialog.SingleButtonCallback() {
@@ -217,9 +241,9 @@ public class CategoryEditActivity extends AppCompatActivity implements View.OnCl
                                     }
                                 })
                                 .show();
-
                     }
                 });
+
                 break;
             case R.id.tb_note_outcome:
                 isIncome = 0;
@@ -230,23 +254,33 @@ public class CategoryEditActivity extends AppCompatActivity implements View.OnCl
                     public void onClick(int position) {
                         CategoryDAO categoryDAO = new CategoryDAOImpl();
                         categoryList = categoryDAO.listCategory();
+                        List<String> outcome_category_name3 = new ArrayList<>();
+                        List<String> income_category_name3 = new ArrayList<>();
                         List<Integer> outcome_category_id3 = new ArrayList<>();
                         List<Integer> income_category_id3 = new ArrayList<>();
+                        String category_name3;
 
                         for (int j = 0; j < categoryList.size(); j++) {
                             Category category = (Category)categoryList.get(j);
                             if (category.getType() == 0) {
+                                outcome_category_name3.add(category.getCategory_name());
                                 outcome_category_id3.add(category.getCategory_id());
                             } else {
+                                income_category_name3.add(category.getCategory_name());
                                 income_category_id3.add(category.getCategory_id());
                             }
                         }
 
-                        Category category = categoryList.get(position);
-                        category_id = outcome_category_id3.get(position);
+                        if (isIncome == 0) {
+                            category_id = outcome_category_id3.get(position);
+                            category_name3 = outcome_category_name3.get(position);
+                        } else {
+                            category_id = income_category_id3.get(position);
+                            category_name3 = income_category_name3.get(position);
+                        }
                         new MaterialDialog.Builder(CategoryEditActivity.this)
                                 .title("修改分类")
-                                .input(category.getCategory_name(), edit_category_name, new MaterialDialog.InputCallback() {
+                                .input(category_name3, null, new MaterialDialog.InputCallback() {
                                     @Override
                                     public void onInput(@NonNull MaterialDialog dialog, CharSequence input) {
                                         edit_category_name = input.toString();
@@ -263,6 +297,7 @@ public class CategoryEditActivity extends AppCompatActivity implements View.OnCl
                                         CategoryDAO categoryDAO = new CategoryDAOImpl();
                                         categoryDAO.updateCategory(category);
                                         initCategory();
+                                        edit_category_name = "";
                                     }
                                 })
                                 .onNeutral(new MaterialDialog.SingleButtonCallback() {
@@ -295,7 +330,7 @@ public class CategoryEditActivity extends AppCompatActivity implements View.OnCl
                 .inputRangeRes(0, 200, R.color.colorPrimaryDark)
                 .input("分类名称", null, (dialog, input) -> {
                     if (input.equals("")) {
-                        Toast.makeText(mContext, "内容不能为空！" + input, Toast.LENGTH_SHORT).show();
+                        Toast.makeText(this, "内容不能为空！" + input, Toast.LENGTH_SHORT).show();
                     } else {
                         //添加新分类
                         Category category = new Category(1, 1, input.toString(), isIncome, state, anchor);
@@ -323,6 +358,13 @@ public class CategoryEditActivity extends AppCompatActivity implements View.OnCl
         List<String> income_category = new ArrayList<>();
         List<Integer> outcome_category_id = new ArrayList<>();
         List<Integer> income_category_id = new ArrayList<>();
+
+        if (outcome_category_id != null) {
+            outcome_category_id.clear();
+        }
+        if (income_category_id != null) {
+            income_category_id.clear();
+        }
 
         for (int j = 0; j < categoryList.size(); j++) {
             Category category = (Category)categoryList.get(j);
