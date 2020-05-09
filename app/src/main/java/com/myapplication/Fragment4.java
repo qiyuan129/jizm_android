a7f9f555f1a74c821c91ac7498b002a8a563bfd2
Chenqiyuan <627064471@qq.com>
2020-05-09 11:53:31 +0800

    将同步功能整合进设置页面；添加同步执行结果的toast提醒

--- app/src/main/java/com/myapplication/Fragment4.java
+++ app/src/main/java/com/myapplication/Fragment4.java
@@ -2,18 +2,35 @@ package com.myapplication;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Looper;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.alibaba.fastjson.JSONObject;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.bitmap.CenterCrop;
 
+import java.io.IOException;
+import java.net.ConnectException;
+import java.net.SocketTimeoutException;
+import java.util.concurrent.TimeUnit;
+
 import jp.wasabeef.glide.transformations.BlurTransformation;
 import jp.wasabeef.glide.transformations.CropCircleTransformation;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
+import util.SyncUtil;
 
 
 public class Fragment4 extends Fragment implements View.OnClickListener{
@@ -26,6 +43,14 @@ public class Fragment4 extends Fragment implements View.OnClickListener{
     ImageView settingEnter;
     TextView userName;
     TextView userTel;
+    TextView syncTextView;
+
+    public static final MediaType MEDIA_TYPE_JSON
+            = MediaType.parse("application/json; charset=utf-8");
+
+    private final OkHttpClient client = new OkHttpClient.Builder()
+                                        .connectTimeout(3, TimeUnit.SECONDS)
+                                        .build();
 
 
     @Override
@@ -57,7 +82,7 @@ public class Fragment4 extends Fragment implements View.OnClickListener{
         settingEnter =(ImageView) mView.findViewById(R.id.setting_enter);
 
 
-
+        syncTextView=(TextView) mView.findViewById(R.id.syncTextView);
         userName=(TextView)mView.findViewById(R.id.user_name);
         userTel=(TextView)mView.findViewById(R.id.user_val);
 
@@ -84,6 +109,7 @@ public class Fragment4 extends Fragment implements View.OnClickListener{
         imageViewh.setOnClickListener(this);
         personalDateEnter.setOnClickListener(this);
         settingEnter.setOnClickListener(this);
+        syncTextView.setOnClickListener(this);
     }
 
 
@@ -110,7 +136,91 @@ public class Fragment4 extends Fragment implements View.OnClickListener{
                 startActivity(intent2);
                 break;
 
+            case R.id.syncTextView: {
+                Toast.makeText(getActivity(),"同步中...请稍候",Toast.LENGTH_SHORT).show();
+                JSONObject syncRecordsJsonObject= SyncUtil.getAllSyncRecords();
+                RequestBody requestBody = RequestBody.create(MEDIA_TYPE_JSON,
+                        syncRecordsJsonObject.toJSONString());
 
+                //这里的主机地址要填电脑的ip地址 ,token要填用户登录时获取的token，超过一定时间会失效，
+                // 需要重新获取
+                Request request = new Request.Builder()
+                        .url("http://192.168.0.100:8080/app/synchronization")
+                        .post(requestBody)
+                        .addHeader("token", "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxIiwiZXhwIjoxNTg4OTMzMjMwfQ.329QAOqCnB6GcqDxwdH9h_Ui7gAwAi4T0MguwnTZ_p8")
+                        .build();
+
+
+                //使用异步请求（用同步发送请求需要在子线程上发起，因为安卓较新的版本都不允许在主线程发送网络请求）
+                client.newCall(request).enqueue(new Callback() {
+                    @Override
+                    public void onFailure(Call call, IOException e) {
+                        if(e instanceof SocketTimeoutException){//判断超时异常
+                            Looper.prepare();
+                            Toast.makeText(getActivity(),"同步错误：网络连接超时",
+                                    Toast.LENGTH_SHORT).show();
+                            Looper.loop();
+                            e.printStackTrace();
+                        }
+                        if(e instanceof ConnectException){//判断连接异常，我这里是报Failed to connect to 10.7.5.144
+                            Looper.prepare();
+                            Toast.makeText(getActivity(),"同步错误：连接到服务器失败",
+                                    Toast.LENGTH_SHORT).show();
+                            Looper.loop();
+                            e.printStackTrace();
+                        }
+                        else{
+                            Looper.prepare();
+                            Toast.makeText(getActivity(),"同步错误：其他错误",
+                                    Toast.LENGTH_SHORT).show();
+                            Looper.loop();
+                            e.printStackTrace();
+                        }
+
+
+                    }
+
+
+                    @Override
+                    public void onResponse(Call call, Response response)  {
+                        try (ResponseBody responseBody = response.body()) {
+                            String responseString=responseBody.string();
+                            JSONObject resultJson=JSONObject.parseObject(responseString);
+                            int statusCode=resultJson.getInteger("code");
+                            String message=resultJson.getString("msg");
+
+                            //如果响应结果状态码为成功的
+                            if(statusCode>=200 && statusCode<400) {
+                                //取出返回的数据；更新本地记录状态
+                                JSONObject dataJson = resultJson.getJSONObject("data");
+                                SyncUtil.processUploadResult(dataJson);
+
+                                Looper.prepare();
+                                Toast.makeText(getActivity(),"已同步",Toast.LENGTH_SHORT)
+                                        .show();
+                                Looper.loop();
+                                return;
+                            }
+                            //响应结果为失败类型
+                            else{
+                                String errorMessage="响应状态码："+statusCode+",错误信息："+message+'\n';
+                                Looper.prepare();
+                                Toast.makeText(getActivity(),"上传本地记录失败\n"+errorMessage,
+                                        Toast.LENGTH_LONG).show();
+                                Looper.loop();
+                                return;
+                            }
+                        } catch (IOException e) {
+                            Looper.prepare();
+                            Toast.makeText(getActivity(),"转换响应结果为字符串时出错",Toast.LENGTH_SHORT)
+                                    .show();
+                            Looper.loop();
+                            e.printStackTrace();
+                        }
+                    }
+                });
+
+            }
                 default:
                     break;
 
